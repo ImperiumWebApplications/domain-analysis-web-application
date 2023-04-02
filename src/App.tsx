fe0bb25b6d086314831e9848dcbc630e3f9c2a73
Aditya <adityasingh220598@gmail.com>
2023-04-02 11:47:29 +0530

    Feature: Putting up a loader and updating configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { TextField, Button, Paper, Box } from "@mui/material";
+import { TextField, Button, Paper, Box, CircularProgress } from "@mui/material";
 import "./App.css";
 
 const App = () => {
@@ -7,10 +7,24 @@ const App = () => {
   const [data, setData] = useState(null);
   const [redirects, setRedirects] = useState([]);
   const [showAnimation, setShowAnimation] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const isValidDomain = (domain: string) => {
     const domainRegex = /^(?!:\/\/)([a-zA-Z0-9-_]+\.){1,}[a-zA-Z]{2,63}$/;
     return domainRegex.test(domain);
+  };
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString("en-US", options as any);
+  };
+
+  const formatDomainAge = (ageInDays: number) => {
+    const years = Math.floor(ageInDays / 365);
+    const months = Math.floor((ageInDays % 365) / 30);
+    const days = ageInDays % 30;
+    return `${years} Years ${months} Months ${days} Days`;
   };
 
   const handleSubmit = async () => {
@@ -29,24 +43,55 @@ const App = () => {
     const hostIoApiKey = import.meta.env.VITE_HOSTIO_API_KEY;
     const hostIoApiUrl = `https://host.io/api/domains/redirects/${domain}?token=${hostIoApiKey}`;
 
+    const completednsApiKey = import.meta.env.VITE_COMPLETE_DNS_API_KEY;
+    const completednsApiUrl = `${corsProxyUrl}http://api.completedns.com/v2/dns-history/${domain}?key=${completednsApiKey}`;
+
+    const whoisApiKey = import.meta.env.VITE_WHOIS_API_KEY;
+    const whoisApiUrl = `https://api.apilayer.com/whois/query?domain=${domain}`;
+    const whoisHeaders = new Headers();
+    whoisHeaders.append("apikey", whoisApiKey);
+
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+      headers: whoisHeaders,
+    };
+    setIsLoading(true);
     try {
       const responses = await Promise.all([
         fetch(domDetailerApiUrl),
         fetch(goDaddyApiUrl),
         fetch(isIndexedApiUrl),
         fetch(hostIoApiUrl),
+        fetch(completednsApiUrl),
+        fetch(whoisApiUrl, requestOptions as any),
       ]);
 
-      const [domDetailerData, goDaddyData, isIndexedData, hostIoData] =
-        await Promise.all(responses.map((response) => response.json()));
+      const [
+        domDetailerData,
+        goDaddyData,
+        isIndexedData,
+        hostIoData,
+        completednsData,
+        whoisData,
+      ] = await Promise.all(responses.map((response) => response.json()));
+
+      const creationDate = new Date(whoisData.result.creation_date);
+      const currentDate = new Date();
+      const ageDifference = currentDate.getTime() - creationDate.getTime();
+      const domainAgeInDays = Math.floor(ageDifference / (1000 * 3600 * 24));
 
       setData({
         ...domDetailerData,
         govalue: goDaddyData.govalue,
         isIndexed: isIndexedData.isIndexed,
+        drops: completednsData.drops,
+        expiration_date: formatDate(whoisData.result.expiration_date),
+        domain_age: formatDomainAge(domainAgeInDays),
       });
 
       setRedirects(hostIoData.domains || []);
+      setIsLoading(false);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -61,6 +106,9 @@ const App = () => {
     "majesticRefDomains",
     "govalue",
     "isIndexed",
+    "drops",
+    "expiration_date",
+    "domain_age",
   ];
 
   useEffect(() => {
@@ -96,7 +144,11 @@ const App = () => {
             </Button>
           </Box>
         </div>
-        {data && (
+        {isLoading ? (
+          <div className="loader-container">
+            <CircularProgress color="secondary" />
+          </div>
+        ) : data && showAnimation ? (
           <div className="result-container">
             {displayParameters.map((parameter, index) => (
               <Paper
@@ -139,7 +191,7 @@ const App = () => {
               </Paper>
             ))}
           </div>
-        )}
+        ) : null}
       </div>
     </div>
   );
